62dd3e769004ed723df7397c054316983f9e88e7
unknown <soloducha.katarzyna@gmail.com>
2021-11-11 22:32:40 +0100

    added unimplemented methods

--- src/main/java/model/Test.java
+++ src/main/java/model/Test.java
@@ -1,4 +1,15 @@
 package model;
 
-public class Test {
+public class Test implements Comparable {
+
+    public boolean isPast(){
+        //todo
+        return false;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        //todo
+        return 0;
+    }
 }
