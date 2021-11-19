d8280fbb4378bee44e708e5a9b61bd944bc1cf96
Szymon Kuś <skus30@wp.pl>
2021-11-19 12:40:30 +0100

    ComparatorByDate Exception in case of null arguments

--- src/main/java/utilities/GradeComparatorByDate.java
+++ src/main/java/utilities/GradeComparatorByDate.java
@@ -7,6 +7,9 @@ import java.util.Comparator;
 public class GradeComparatorByDate implements Comparator<Grade> {
     @Override
     public int compare(Grade o1, Grade o2) {
+        if (o1 == null || o2 == null) {
+            throw new IllegalArgumentException("Grade passed to the comparator should not be null");
+        }
         if (o1.getDate().isAfter(o2.getDate())) {
             return 1;
         } else if (o1.getDate().isBefore(o2.getDate())) {
