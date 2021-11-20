ff5c45695c0d5544a548afc746d968b872f9d0fe
AgataLa <agatalachowiecka@gmail.com>
2021-11-20 13:32:04 +0100

    implemented SchoolTest class

--- src/main/java/model/SchoolTest.java
+++ src/main/java/model/SchoolTest.java
@@ -1,21 +1,62 @@
 package model;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-public class SchoolTest implements Comparable {
+public class SchoolTest implements Comparable<SchoolTest> {
 
-    public void setDate(LocalDate date) {
-        //todo
+    private String name;
+    private LocalDateTime date;
+    private String description;
+
+    public SchoolTest() {
+        this(null, null, null);
+    }
+
+    public SchoolTest(String name, LocalDateTime date, String description) {
+        this.name = name;
+        this.date = date;
+        this.description = description;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public boolean isPast(){
-        //todo
-        return false;
+        return LocalDateTime.now().isAfter(date);
     }
 
     @Override
-    public int compareTo(Object o) {
-        //todo
-        return 0;
+    public int compareTo(SchoolTest otherTest) {
+        if(otherTest == null) {
+            throw new IllegalArgumentException("Cannot compare to null argument.");
+        }
+        if(this.date.isBefore(otherTest.getDate())) {
+            return -1;
+        } else if(this.date.isAfter(otherTest.getDate())) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
