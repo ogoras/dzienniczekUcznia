9fe7a181caf0a319db4b69ea814f905fadea5a0f
7Bubun <62255561+7Bubun@users.noreply.github.com>
2021-12-04 18:58:09 +0100

    Enhance user interface

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -29,6 +29,18 @@ public class Repository {
         student.setAttendance(chemistry, chemistryAttendance);
         student.addSubject(chemistry);
 
+        Subject physicalEducation = new Subject("PE");
+        int PEAttendance = 1;
+        physicalEducation.setAmountOfClasses(4);
+        student.setAttendance(physicalEducation, PEAttendance);
+        student.addSubject(physicalEducation);
+
+        Subject physics = new Subject("Physics");
+        int physicsAttendance = 3;
+        physics.setAmountOfClasses(8);
+        student.setAttendance(physics, physicsAttendance);
+        student.addSubject(physics);
+
         Grade mathGrade1 = new Grade(6, 2, math);
         Grade mathGrade2 = new Grade(3, 3, math);
         Grade mathGrade3 = new Grade(2, 1, math);
@@ -46,14 +58,30 @@ public class Repository {
         Grade chemistryGrade2 = new Grade(1, 1, chemistry);
         Grade chemistryGrade3 = new Grade(6, 4, chemistry);
         Grade chemistryGrade4 = new Grade(2, 4, chemistry);
-        chemistryGrade1.setDate(LocalDate.of(2021,10,20));
-        chemistryGrade2.setDate(LocalDate.of(2020,11,28));
-        chemistryGrade3.setDate(LocalDate.of(2021,1,18));
-        chemistryGrade4.setDate(LocalDate.of(2021,1,1));
+        chemistryGrade1.setDate(LocalDate.of(2021, 10, 20));
+        chemistryGrade2.setDate(LocalDate.of(2020, 11, 28));
+        chemistryGrade3.setDate(LocalDate.of(2021, 1, 18));
+        chemistryGrade4.setDate(LocalDate.of(2021, 1, 1));
         student.addGrade(chemistryGrade1);
         student.addGrade(chemistryGrade2);
         student.addGrade(chemistryGrade3);
         student.addGrade(chemistryGrade4);
+
+        Grade physicsGrade0 = new Grade(1, 5, physics);
+        Grade physicsGrade1 = new Grade(2, 3, physics);
+        Grade physicsGrade2 = new Grade(1, 2, physics);
+        physicsGrade0.setDate(LocalDate.of(2020, 12, 2));
+        physicsGrade0.setDate(LocalDate.of(2020, 12, 1));
+        physicsGrade0.setDate(LocalDate.of(2020, 11, 15));
+        student.addGrade(physicsGrade0);
+        student.addGrade(physicsGrade1);
+        student.addGrade(physicsGrade2);
+
+        Grade PEGrade1 = new Grade(4, 2, physicalEducation);
+        Grade PEGrade2 = new Grade(6, 4, physicalEducation);
+        PEGrade1.setDate(LocalDate.of(2020, 11, 21));
+        student.addGrade(PEGrade1);
+        student.addGrade(PEGrade2);
 
         SchoolTest test1 = new SchoolTest("DNA Exam",
                 LocalDateTime.of(2010, 8, 17, 10, 5),
--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,5 +1,6 @@
 package ui;
 
+import controller.Controller;
 import model.Grade;
 import model.SchoolTest;
 import model.Student;
@@ -11,27 +12,32 @@ import utilities.GradeComparatorByWeight;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class UserInterface {
 
-    Student student;
-    BufferedReader reader;
-    String command;
-    Comparator<Grade> gradeComparator;
-    boolean sortDescending=true;
-    boolean isSubjectDisplayed=false;
-    Subject subject;
+    private Student student;
+    private BufferedReader reader;
+    private String command;
+    private Comparator<Grade> gradeComparator;
+    private boolean sortDescending = true;
+    private boolean isSubjectDisplayed = false;
+    private Subject subject;
+    private List<String> commandsWithDescription;
 
-    UserInterface(){
+    UserInterface() {
         Repository repository = new Repository();
         gradeComparator = new GradeComparatorByDate();
         gradeComparator = gradeComparator.reversed();
         student = repository.getStudent(0);
+
+        commandsWithDescription = new ArrayList<>();
+        initCommandsList();
     }
 
-    public void start(){
+    public void start() {
         reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
@@ -41,18 +47,18 @@ public class UserInterface {
             try {
                 command = reader.readLine();
                 String[] args = command.split(" ");
-                switch(args[0]) {
+                switch (args[0]) {
                     case "subject":
                     case "sub":
                         String name;
-                        if(args.length > 1) {
+                        if (args.length > 1) {
                             name = args[1];
                         } else {
                             System.out.println("Enter subject name: ");
                             name = reader.readLine();
                         }
                         subject = student.getSubject(name);
-                        if(subject != null) {
+                        if (subject != null) {
                             isSubjectDisplayed = true;
                             displaySubject(subject);
                         } else {
@@ -60,6 +66,11 @@ public class UserInterface {
                         }
                         break;
                     case "sort":
+                        if (args.length < 2) {
+                            System.out.println("Missing argument");
+                            break;
+                        }
+
                         switch (args[1]) {
                             case "ascending", "asc" -> {
                                 gradeComparator = sortDescending ? gradeComparator.reversed() : gradeComparator;
@@ -78,13 +89,26 @@ public class UserInterface {
                                 gradeComparator = sortDescending ? gradeComparator.reversed() : gradeComparator;
                             }
                         }
-                        if (isSubjectDisplayed)
+                        if (isSubjectDisplayed) {
                             displaySubject(subject);
+                        } else {
+                            System.out.println("You have to choose subject");
+                        }
+
+                        break;
+                    case "help":
+                        System.out.println("Available command: ");
+
+                        for (String s : commandsWithDescription) {
+                            System.out.println(s);
+                        }
+
+                        isSubjectDisplayed = false;
                         break;
                     case "home":
                     case "main":
                         displayMain();
-                        isSubjectDisplayed=false;
+                        isSubjectDisplayed = false;
                     case "exit":
                         break;
                     default:
@@ -96,11 +120,44 @@ public class UserInterface {
         }
     }
 
-    private void displayMain(){
-        System.out.println("Hi " + student.getFirstname() + "!");
-        System.out.println("Your general average grade is: " +
-                round(student.calculateArithmeticAverage(),2));
-        System.out.println("Your general attendance is: " + round(student.calculateAttendance(),2));
+    private void initCommandsList() {
+        commandsWithDescription.add("subject/sub - gives a prompt to insert name of subject to display");
+        commandsWithDescription.add("sort ascending/asc - sorts subject grades ascending value*");
+        commandsWithDescription.add("sort descending/desc - sorts subject grades descending value*");
+        commandsWithDescription.add("sort date - sorts subject grades date*");
+        commandsWithDescription.add("sort weight - sorts subject grades date*");
+        commandsWithDescription.add("home/main - display homepage");
+        commandsWithDescription.add("help - display available command (current view)");
+        commandsWithDescription.add("exit - close program");
+        commandsWithDescription.add("*works only in subject view");
+    }
+
+    private void displayMain() {
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_RED = "\u001B[31m";
+        double average = round(student.calculateArithmeticAverage(), 2);
+        double attendance = round(student.calculateAttendance(), 2);
+        List<Subject> lowAverageSubjects = Controller.getListOfTroublesomeSubjects(student);
+        List<Subject> lowAttendanceSubjects = Controller.getListOfMostMissedSubjects(student);
+
+
+        System.out.println("Hi " + student.getFirstname() + "!\n");
+        System.out.println("Your general average grade is: " + average);
+
+        if (!lowAverageSubjects.isEmpty()) {
+            for (Subject s : lowAverageSubjects) {
+                System.out.println(ANSI_RED + "Your " + s.getName() + "'s grades are too low" + ANSI_RESET);
+            }
+        }
+
+        System.out.println("\nYour general attendance is: " + attendance);
+
+        if (!lowAttendanceSubjects.isEmpty()) {
+            for (Subject s : lowAttendanceSubjects) {
+                System.out.println(ANSI_RED + "Your " + s.getName() + "'s attendance is too low" + ANSI_RESET);
+            }
+        }
+
         System.out.println();
         displaySubjectList();
         System.out.println();
@@ -109,50 +166,70 @@ public class UserInterface {
 
     private void displayTestList() {
         System.out.println("Your upcoming tests:");
-        System.out.println("Test:\t\t\tSubject:\t\tDate:\t\t\t\t\tDescription:");
-        for(SchoolTest test : student.getUpcomingTests()) {
+        //System.out.println("Test:\t\t\tSubject:\t\tDate:\t\t\t\t\tDescription:");
+        System.out.println(String.format("%-20s %-20s %-20s %s", "Test", "Subject", "Date", "Description"));
+        for (SchoolTest test : student.getUpcomingTests()) {
             printTest(test);
         }
     }
 
     private void printTest(SchoolTest test) {
-        System.out.println(test.getName() +
-                "\t" + test.getSubject().getName() +
-                "\t\t\t" + test.getDate() +
-                "\t\t" + test.getDescription() );
+        System.out.println(String.format(
+                "%-20s %-20s %-20s %s",
+                test.getName(),
+                test.getSubject().getName(),
+                test.getDate(),
+                test.getDescription()
+        ));
     }
 
     private void displaySubjectList() {
-        System.out.println("Subject:\t\tAverage Grade:\t\tAttendance:");
-        for(Subject sub: student.getSubjects()) {
-            System.out.println(sub.getName() +
-                    "\t\t" + round(student.calculateWeightedAverage(sub),2) +
-                    "\t\t\t\t" + round(student.calculateSubjectAttendance(sub)*100,2) + "%");
+        System.out.println(String.format("%-15s %-15s %-15s", "Subject", "Average grade", "Attendance"));
+        for (Subject sub : student.getSubjects()) {
+            System.out.println(String.format(
+                    "%-15s %-15s %-15s",
+                    sub.getName(),
+                    round(student.calculateWeightedAverage(sub), 2),
+                    round(student.calculateSubjectAttendance(sub) * 100, 2) + "%"
+            ));
         }
     }
 
-    private void displaySubject(Subject s){
+    private void displaySubject(Subject s) {
         System.out.println(s.getName());
         System.out.println();
         System.out.println("Grades: ");
-        System.out.println("Value:\tWeight:\tDate:\t\t\tTest:\t\t\tComment:");
+        System.out.println(String.format(
+                "%-15s %-15s %-15s %-15s %s",
+                "Value:",
+                "Weight:",
+                "Date:",
+                "Test:",
+                "Comment:"
+        ));
+
         List<Grade> grades = student.getGrades(s);
         grades.sort(gradeComparator);
-        for(Grade grade: grades) {
-            System.out.println(grade.getValue() + "\t\t" +
-                                grade.getWeight() + "\t\t" +
-                                grade.getDate() + "\t\t" +
-                                (grade.getTest()==null?"":grade.getTest()) + "\t\t" +
-                                grade.getComment());
+        for (Grade grade : grades) {
+            System.out.println(String.format(
+                    "%-15s %-15s %-15s %-15s %s",
+                    grade.getValue(),
+                    grade.getWeight(),
+                    grade.getDate(),
+                    grade.getTest() == null ? "" : grade.getTest(),
+                    grade.getComment()
+            ));
         }
 
         System.out.println();
         System.out.println("Attendance: " + student.getAttendances().get(s) + "/" + s.getAmountOfClasses() + " = "
-                + round(student.calculateSubjectAttendance(s)*100, 2) + "%");
+                + round(student.calculateSubjectAttendance(s) * 100, 2) + "%");
 
         System.out.println();
-        if (!s.getTests().isEmpty())
-            System.out.println("Test:\t\t\tSubject:\t\tDate:\t\t\t\t\tDescription:");
+        if (!s.getTests().isEmpty()) {
+            System.out.println(String.format("%-20s %-20s %-20s %s", "Test", "Subject", "Date", "Description"));
+        }
+
         for (SchoolTest test : s.getTests()) {
             printTest(test);
         }
@@ -160,9 +237,9 @@ public class UserInterface {
 
     private double round(double value, int decimalPlaces) {
         int factor = 1;
-        for (int i=0; i<decimalPlaces;  i++) {
+        for (int i = 0; i < decimalPlaces; i++) {
             factor *= 10;
         }
-        return Math.round(value*factor)/(double)factor;
+        return Math.round(value * factor) / (double) factor;
     }
 }
