9a6b85bef032842f4904fe38ad80924d8ab2aba3
Katarzyna Sołoducha <soloducha.katarzyna@gmail.com>
2021-12-09 22:57:05 +0100

    added benchmarks for controller methods

---
+++ src/main/java/benchmarks/controller/ListOfTroublesomeSubjectsBenchmark.java
@@ -0,0 +1,45 @@
+package benchmarks.controller;
+
+import controller.Controller;
+import model.Grade;
+import model.Student;
+import model.Subject;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+public class ListOfTroublesomeSubjectsBenchmark {
+
+    private Student student;
+
+    @Param({"10", "100"})
+    int numberOfGradesInSubject;
+
+    @Param({"5", "20"})
+    int numberOfTroublesomeSubjects;
+
+    @Setup
+    public void setup(){
+        student  = new Student("Jan", "Kowalski", 1234);
+
+        for(int i = 0; i < numberOfTroublesomeSubjects; i++){
+            Subject subject = new Subject("Subject" + i);
+
+            for(int j = 0; j < numberOfTroublesomeSubjects; j++){
+                student.addGrade(new Grade(1, 5, subject));
+            }
+        }
+    }
+
+    @Fork(value = 1, warmups = 1)
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = 1, timeUnit = TimeUnit.SECONDS, time = 1)
+    @Measurement(iterations = 2, timeUnit = TimeUnit.SECONDS, time = 1)
+    public List<Subject> getStudentsListOfTroublesomeSubjects() {
+        return Controller.getListOfTroublesomeSubjects(student);
+    }
+}
